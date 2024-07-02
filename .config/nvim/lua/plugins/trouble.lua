bdb7b4b037357aee6a445538b7af1a0cd241f5cb
Ovsiankina <ovsiankina@proton.me>
2024-07-02 08:29:43 +0200

    Fix(nvim, trouble): Fix EOL error from trouble dev branch
    Trouble was removed and reinistaed to ensure the the correct branch is pulled. The comments in trouble.lua were organized

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -4,34 +4,34 @@ return {
   keys = {
     {
       "<leader>xx",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
+      "<cmd>TroubleToggle document_diagnostics<cr>",
+      desc = "Document Diagnostics (Trouble)",
     },
     {
       "<leader>x",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
+      "<cmd>TroubleToggle workspace_diagnostics<cr>",
+      desc = "Workspace Diagnostics (Trouble)",
     },
-    -- {
-    --   "<leader>cs",
-    --   "<cmd>Trouble symbols toggle focus=false<cr>",
-    --   desc = "Symbols (Trouble)",
-    -- },
-    -- {
-    --   "<leader>cl",
-    --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-    --   desc = "LSP Definitions / references / ... (Trouble)",
-    -- },
     {
       "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
+      "<cmd>TroubleToggle loclist<cr>",
       desc = "Location List (Trouble)",
     },
     {
       "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
+      "<cmd>TroubleToggle quickfix<cr>",
       desc = "Quickfix List (Trouble)",
     },
   },
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {}, -- Use default options or add custom configurations if needed
 }
+-- {
+--   "<leader>cs",
+--   "<cmd>Trouble symbols toggle focus=false<cr>",
+--   desc = "Symbols (Trouble)",
+-- },
+-- {
+--   "<leader>cl",
+--   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+--   desc = "LSP Definitions / references / ... (Trouble)",
+-- },
