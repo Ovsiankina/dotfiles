687840b266dbf4baeed94a4035404821488b68db
Ovsiankina <ovsiankina@proton.me>
2024-07-03 08:56:52 +0200

    Feat(nvim): vim-startuptime plugin added

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -4,6 +4,7 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
+		-- require("alpha").setup(require("alpha.themes.dashboard").config)
+    require'alpha'.setup(require'alpha.themes.startify'.config)
 	end,
 }
