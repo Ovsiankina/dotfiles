905ec0787709f7b4a79582bdd63729b057632f88
Ovsiankina <ovsiankina@proton.me>
2024-07-03 08:21:26 +0200

    Feat(nvim): ts-comments plugin added

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -1,31 +1,39 @@
 return {
-	"LudoPinelli/comment-box.nvim",
-	config = function()
-		-- ── Named parts ─────────────────────────────────────────────────────
+  {
+    "LudoPinelli/comment-box.nvim",
+    config = function()
+      -- ── Named parts ─────────────────────────────────────────────────────
 
-		-- Simple line
+      -- Simple line
 
-		-- ──────────────────────────────────────────────────────────────────────
+      -- ──────────────────────────────────────────────────────────────────────
 
-		--                                                           ▲
-		--   Marked comments                                         █
-		--                                                           ▼
+      --                                                           ▲
+      --   Marked comments                                         █
+      --                                                           ▼
 
-		-- Remove comment box or normal comment
+      -- Remove comment box or normal comment
 
-		local wk = require("which-key")
-		wk.register({
-			C = {
-				mode = { "n", "v" },
-				silent = true, -- use `silent` when creating keymaps
-				noremap = true, -- use `noremap` when creating keymaps
-				name = " □  Boxes",
-				b = { "<Cmd>CBccbox<CR>", "Box Title" },
-				t = { "<Cmd>CBllline<CR>", "Titled Line" },
-				l = { "<Cmd>CBline<CR>", "Simple Line" },
-				m = { "<Cmd>CBllbox14<CR>", "Marked" },
-				d = { "<Cmd>CBd<CR>", "Remove a box" },
-			},
-		}, { prefix = "<leader>" })
-	end,
+      local wk = require("which-key")
+      wk.register({
+        C = {
+          mode = { "n", "v" },
+          silent = true, -- use `silent` when creating keymaps
+          noremap = true, -- use `noremap` when creating keymaps
+          name = " □  Boxes",
+          b = { "<Cmd>CBccbox<CR>", "Box Title" },
+          t = { "<Cmd>CBllline<CR>", "Titled Line" },
+          l = { "<Cmd>CBline<CR>", "Simple Line" },
+          m = { "<Cmd>CBllbox14<CR>", "Marked" },
+          d = { "<Cmd>CBd<CR>", "Remove a box" },
+        },
+      }, { prefix = "<leader>" })
+    end,
+  },
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  },
 }
