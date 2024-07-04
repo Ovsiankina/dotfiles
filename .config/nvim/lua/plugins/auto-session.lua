e28a155c770047758e75ecc16dd868aab046a6b0
Ovsiankina <ovsiankina@proton.me>
2024-07-04 10:49:54 +0200

    Feat(nvim): New plugins for mini.nvim added

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,15 +1,16 @@
 return {
-	{
-		"rmagatti/auto-session",
-		dependencies = {
-			"nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
-		},
-		config = function()
-			require("auto-session").setup({
-				auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
-			})
-		end,
-	},
-  {"rmagatti/session-lens"},
-   dependencies = {'rmagatti/auto-session', 'nvim-telescope/telescope.nvim'},
+  {
+    "rmagatti/auto-session",
+    dependencies = {
+      "nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
+    },
+    config = function()
+      require("auto-session").setup({
+        auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+      })
+    end,
+  },
+  -- TODO: add keymap for autosession telescope
+  { "rmagatti/session-lens" },
+  dependencies = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
 }
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,4 +1,7 @@
 return {
-  { 'echasnovski/mini.nvim', version = false }
-
+  { 'echasnovski/mini.nvim', version = false },
+  -- Auto complete the missing pairs for e.g () {} []
+  { 'echasnovski/mini.pairs', version = false },
+  -- Improve the textobject e.g inside content of () {} []
+  { 'echasnovski/mini.ai', version = false },
 }
