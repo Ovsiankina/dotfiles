e30adfe4596b575c21e40690590c8e55afafa989
Ovsiankina <ovsiankina@proton.me>
2024-07-05 09:02:18 +0200

    feat(nvim): Git diff keymap added and some quality of life modification

--- .config/nvim/lua/plugins/git-integration.lua
+++ .config/nvim/lua/plugins/git-integration.lua
@@ -1,43 +1,45 @@
 return {
-	-- Git commands integrated into neovim
-	{
-		"tpope/vim-fugitive",
-	},
-	-- Previews, blame, etc..
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
+  -- Git commands integrated into neovim
+  {
+    "tpope/vim-fugitive",
+  },
+  -- Previews, blame, etc..
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
 
-			--          ╭─────────────────────────────────────────────────────────╮
-			--          │                          Remap                          │
-			--          ╰─────────────────────────────────────────────────────────╯
+      --          ╭─────────────────────────────────────────────────────────╮
+      --          │                          Remap                          │
+      --          ╰─────────────────────────────────────────────────────────╯
 
-			local wk = require("which-key")
-			wk.register({
-				g = {
-					mode = { "n" },
-					name = " Git",
-					c = { ":Git ", "Short(c)ut for :Git in CMD" },
-					h = { ":Gitsigns preview_hunk<CR>", "Preview (h)unk" },
-					b = {
-						name = " Blame",
-						l = { ":Gitsigns toggle_current_line_blame<CR>", "Blame current (L)ine" },
-					},
-					v = {
-						name = " Visualisation",
-						b = { ":Flog<CR>", " (B)ranches" },
-						s = { ":Flogsplit<CR>", " (S)plit" },
-						g = { ":Floggit<CR>", " (G)it" },
-					},
-				},
-			}, { prefix = "<leader>" })
-		end,
-	},
-	-- Git branches visualisation (:Flog)
-	{
-		"rbong/vim-flog",
-		lazy = true,
-		cmd = { "Flog", "Flogsplit", "Floggit" },
-	},
+      local wk = require("which-key")
+      wk.register({
+        g = {
+          mode = { "n" },
+          name = " Git",
+          A = { ":Git add .<CR>", "Git (A)dd ." },
+          C = { ":Git commit<CR>", "Git (C)ommit" },
+          h = { ":Gitsigns preview_hunk<CR>", "(p)review (h)unk" },
+          r = { ":Gitsigns reset_hunk<CR>", "(r)eset (h)unk" },
+          D = { ":Gitsigns diffthis<CR>", "(D)iff this" },
+          d = { ":Gdiffsplit<CR>", " (d)iff split: working tree vs. staged " },
+          b = { ":Gitsigns toggle_current_line_blame<CR>", "Blame current (L)ine" },
+
+          v = {
+            name = " Visualisation",
+            b = { ":Flog<CR>", " (b)ranches" },
+            s = { ":Flogsplit<CR>", " (s)plit" },
+            g = { ":Floggit<CR>", " (g)it" },
+          },
+        },
+      }, { prefix = "<leader>" })
+    end,
+  },
+  -- Git branches visualisation (:Flog)
+  {
+    "rbong/vim-flog",
+    lazy = true,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+  },
 }
