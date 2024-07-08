027a831af404c95883518a8b816b473cb178a4b9
Ovsiankina <ovsiankina@proton.me>
2024-07-08 08:38:01 +0200

    Feat(nvim): neogit plugin added

--- .config/nvim/lua/plugins/git-integration.lua
+++ .config/nvim/lua/plugins/git-integration.lua
@@ -1,44 +1,56 @@
 return {
-  -- Git commands integrated into neovim
-  {
-    "tpope/vim-fugitive",
-  },
-  -- Previews, blame, etc..
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
+	-- Git commands integrated into neovim
+	{
+		"tpope/vim-fugitive",
+	},
+	-- Previews, blame, etc..
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 
-      --          ╭─────────────────────────────────────────────────────────╮
-      --          │                          Remap                          │
-      --          ╰─────────────────────────────────────────────────────────╯
+			--          ╭─────────────────────────────────────────────────────────╮
+			--          │                          Remap                          │
+			--          ╰─────────────────────────────────────────────────────────╯
 
-      local wk = require("which-key")
-      wk.register({
-        ["<leader>g"] = {
-          name = "Git",
-          ["A"] = { mode = "n", "<cmd>Git add .<CR>", "Git [A]dd ." },
-          ["C"] = { mode = "n", "<cmd>Git commit<CR>", "Git [C]ommit" },
-          ["h"] = { mode = "n", "<cmd>Gitsigns preview_hunk<CR>", "[p]review hunk" },
-          ["r"] = { mode = "n", "<cmd>Gitsigns reset_hunk<CR>", "[r]eset hunk" },
-          ["D"] = { mode = "n", "<cmd>Gitsigns diffthis<CR>", "[D]iff this" },
-          ["d"] = { mode = "n", "<cmd>Gdiffsplit<CR>", "[d]iff split: working tree vs. staged " },
-          ["b"] = { mode = "n", "<cmd>Gitsigns toggle_current_line_blame<CR>", "[b]lame current line" },
-        },
+			local wk = require("which-key")
+			wk.register({
+				["<leader>g"] = {
+					name = "Git",
+					["A"] = { mode = "n", "<cmd>Git add .<CR>", "Git [A]dd ." },
+					["C"] = { mode = "n", "<cmd>Git commit<CR>", "Git [C]ommit" },
+					["h"] = { mode = "n", "<cmd>Gitsigns preview_hunk<CR>", "[p]review hunk" },
+					["r"] = { mode = "n", "<cmd>Gitsigns reset_hunk<CR>", "[r]eset hunk" },
+					["D"] = { mode = "n", "<cmd>Gitsigns diffthis<CR>", "[D]iff this" },
+					["d"] = { mode = "n", "<cmd>Gdiffsplit<CR>", "[d]iff split: working tree vs. staged " },
+					["b"] = { mode = "n", "<cmd>Gitsigns toggle_current_line_blame<CR>", "[b]lame current line" },
+				},
 
-        ["<leader>gv"] = {
-          name = " Visualisation",
-          ["b"] = { mode = "n", "<cmd>Flog<CR>", " (b)ranches" },
-          ["s"] = { mode = "n", "<cmd>Flogsplit<CR>", " (s)plit" },
-          ["g"] = { mode = "n", "<cmd>Floggit<CR>", " (g)it" },
-        },
-      })
-    end,
-  },
-  -- Git branches visualisation (:Flog)
-  {
-    "rbong/vim-flog",
-    lazy = true,
-    cmd = { "Flog", "Flogsplit", "Floggit" },
-  },
+				["<leader>gv"] = {
+					name = " Visualisation",
+					["b"] = { mode = "n", "<cmd>Flog<CR>", " (b)ranches" },
+					["s"] = { mode = "n", "<cmd>Flogsplit<CR>", " (s)plit" },
+					["g"] = { mode = "n", "<cmd>Floggit<CR>", " (g)it" },
+				},
+			})
+		end,
+	},
+	-- Git branches visualisation (:Flog)
+	{
+		"rbong/vim-flog",
+		lazy = true,
+		cmd = { "Flog", "Flogsplit", "Floggit" },
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+	},
 }
