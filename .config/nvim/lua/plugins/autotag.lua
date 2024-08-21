3a06196c6020d1f9dc5c3dd1169330c38a3abd16
MoDaORIF <david.mostoslavski@formation.orif.ch>
2024-08-21 11:18:59 +0200

    fix(nvim): autotag plugin fix.
    Now closes tags and renames them correctly

--- .config/nvim/lua/plugins/autotag.lua
+++ .config/nvim/lua/plugins/autotag.lua
@@ -1,5 +1,24 @@
 return {
-  -- FIX: Not working
-  -- NOTE: nvim-ts-autotag will not work unless you have treesitter parsers (like html) installed for a given filetype.
-  "windwp/nvim-ts-autotag"
+	-- FIX: Not working
+	-- NOTE: nvim-ts-autotag will not work unless you have treesitter parsers (like html) installed for a given filetype.
+
+	"windwp/nvim-ts-autotag",
+	config = function()
+		require("nvim-ts-autotag").setup({
+			opts = {
+				-- Defaults
+				enable_close = true, -- Auto close tags
+				enable_rename = true, -- Auto rename pairs of tags
+				enable_close_on_slash = true, -- Auto close on trailing </
+			},
+			-- Also override individual filetype configs, these take priority.
+			-- Empty by default, useful if one of the "opts" global settings
+			-- doesn't work well in a specific filetype
+			per_filetype = {
+				["html"] = {
+					enable_close = true,
+				},
+			},
+		})
+	end,
 }
