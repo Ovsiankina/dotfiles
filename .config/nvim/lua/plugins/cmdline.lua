51c3f829199a0d6d2acb5856ca600baa239cf8d0
ovsiankina <ovsiankina@proton.me>
2024-10-22 10:57:23 +0200

    nvim(lualine): lsp progress added to lualine

--- .config/nvim/lua/plugins/cmdline.lua
+++ .config/nvim/lua/plugins/cmdline.lua
@@ -1,15 +1,47 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
-	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
-	},
+    "f#olke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- add any #options here
+    },
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+    },
+    require("noice").setup({
+        views = {
+            cmdline_popup = {
+                border = {
+                    style = "none",
+                    padding = { 1, 2 },
+                },
+                filter_options = {},
+                win_options = {
+                    winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+                },
+            },
+        },
+        popupmenu = {
+            relative = "editor",
+            position = {
+                row = 8,
+                col = "50%",
+            },
+            size = {
+                width = 60,
+                height = 10,
+            },
+            border = {
+                style = "rounded",
+                padding = { 0, 1 },
+            },
+            win_options = {
+                winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            },
+        },
+    }),
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,25 +1,33 @@
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "catppuccin",
-                },
-                sections = {
-                    lualine_a = { "mode" },
-                    lualine_b = { "branch", "diff", "diagnostics" },
-                    lualine_c = { "filename", require("lsp-status").status() },
-                    -- BUG: lsp-status doesn't show up in lualine
-                    lualine_x = { "encoding", "fileformat", "filetype" },
-                    lualine_y = { "progress" },
-                    lualine_z = { "location" },
-                },
-                extensions = { "trouble" },
-            })
-        end,
-    },
-    {
-        "arkav/lualine-lsp-progress",
-    },
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin",
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = {
+						"filename",
+						require("lsp-status").status(),
+						{
+							require("noice").api.statusline.mode.get,
+							cond = require("noice").api.statusline.mode.has,
+							color = { fg = "#ff9e64" },
+						},
+						"lsp_progress", -- Add lsp_progress here
+					},
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				extensions = { "trouble" },
+			})
+		end,
+	},
+	{
+		"arkav/lualine-lsp-progress",
+	},
 }
