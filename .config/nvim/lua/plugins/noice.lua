f635c4c3ac538bd0c51366a91c4775b30870c265
ovsiankina <ovsiankina@proton.me>
2024-12-11 09:19:21 +0100

    fix(nvim): noice filters long messages and 'redirects' them to :messages

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -2,7 +2,6 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-        -- add any options here
         views = {
             cmdline_popup = {
                 border = {
@@ -33,13 +32,28 @@ return {
                 winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
             },
         },
+        routes = {
+            -- Ensure long messages go to `:messages`
+            {
+                filter = {
+                    event = "msg_show",
+                    min_height = 5, -- Messages taller than 5 lines
+                },
+                view = "split", -- Use the split view for long messages
+            },
+        },
+        lsp = {
+            progress = { enabled = true },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
     },
     dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
         "rcarriga/nvim-notify",
     },
 }
+
