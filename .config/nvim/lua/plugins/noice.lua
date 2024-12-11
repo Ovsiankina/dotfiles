fe1a7905bfe0e3acaae8fb731d243f92ad770fcb
ovsiankina <ovsiankina@proton.me>
2024-12-11 12:00:28 +0100

    fix(nvim): Vimtex now works proprely

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,59 +1,70 @@
+-- BUG: VimTex errors doesn't display proprely. It's truncated and unaccessible
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        views = {
-            cmdline_popup = {
-                border = {
-                    style = "none",
-                    padding = { 1, 2 },
-                },
-                filter_options = {},
-                win_options = {
-                    winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-                },
-            },
-        },
-        popupmenu = {
-            relative = "editor",
-            position = {
-                row = 8,
-                col = "50%",
-            },
-            size = {
-                width = 60,
-                height = 10,
-            },
-            border = {
-                style = "rounded",
-                padding = { 0, 1 },
-            },
-            win_options = {
-                winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-            },
-        },
-        routes = {
-            -- Ensure long messages go to `:messages`
-            {
-                filter = {
-                    event = "msg_show",
-                    min_height = 5, -- Messages taller than 5 lines
-                },
-                view = "split", -- Use the split view for long messages
-            },
-        },
-        lsp = {
-            progress = { enabled = true },
-            override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true,
-            },
-        },
-    },
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
-    },
-}
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		views = {
+			cmdline_popup = {
+				border = {
+					style = "none",
+					padding = { 1, 2 },
+				},
+				filter_options = {},
+				win_options = {
+					winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+				},
+			},
+		},
+		popupmenu = {
+			relative = "editor",
+			position = {
+				row = 8,
+				col = "50%",
+			},
+			size = {
+				width = 60,
+				height = 10,
+			},
+			border = {
+				style = "rounded",
+				padding = { 0, 1 },
+			},
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+			},
+		},
+		routes = {
+			-- Ensure long messages go to `:messages`
+			{
+				filter = {
+					event = "msg_show",
+					min_height = 4, -- Messages taller than 5 lines
+				},
+				view = "split", -- Use the split view for long messages
+				-- Skip VimTeX error messages or notifications
+			},
 
+			{
+				filter = {
+					event = "notify", -- The event for notifications
+					opts = {
+						title = "VimTeX:", -- Target VimTeX messages (adjust title if needed)
+					},
+				},
+				opts = { skip = true }, -- Skip processing these messages by Noice
+			},
+		},
+		lsp = {
+			progress = { enabled = true },
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+}
--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,14 +3,19 @@ return {
 		"lervag/vimtex",
 		lazy = false, -- we don't want to lazy load VimTeX
 
-		init = function()
+		-- NOTE: if doesn't work: try `$ fmtutil-sys --all`
+		-- `$ sudo texconfig rehash`
+		-- `$ sudo pacman -S extra/texlive-latex extra/texlive-latexextra extra/texlive-plaingeneric zathura-pdf-mupdf`
 
-            vim.g.maplocalleader = "\\"
+		init = function()
+			vim.g.maplocalleader = "\\"
+
+            vim.g.vimtex_compiler_latexmk_options = "-pdf -quiet"
 
 			-- VimTeX configuration goes here, e.g.
 			vim.g.vimtex_view_method = "zathura"
 			vim.g.vimtex_compiler_method = "latexmk"
-			vim.g.vimtex_view_general_viewer = "okular" -- Change to your viewer if needed
+			vim.g.vimtex_view_general_viewer = "zathura" -- Change to your viewer if needed
 			vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex" -- PDF viewer options
 		end,
 	},
