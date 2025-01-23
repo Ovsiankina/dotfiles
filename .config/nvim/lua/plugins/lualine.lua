fcf2c4b79e5ce83d0c176ea5f4ffa68d537cb6ad
ovsiankina <ovsiankina@proton.me>
2025-01-23 09:34:38 +0100

    fix: lualine lsp status now shows proprely

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -18,27 +18,47 @@ return {
                 -- 	},
                 -- 	"lsp_progress", -- Add lsp_progress here
                 -- },
+                -- lualine_c = {
+                --     "filename",
+                --     require("lsp-status").status(),
+                --     {
+                --         function()
+                --             local noice_status = require("noice").api.statusline.mode.get()
+                --             -- Truncate noice status if it's too long
+                --             return noice_status and #noice_status > 50 and noice_status:sub(1, 47) .. "..."
+                --                 or noice_status
+                --         end,
+                --         cond = require("noice").api.statusline.mode.has,
+                --         color = { fg = "#ff9e64" },
+                --     },
+                --     {
+                --         function()
+                --             -- Truncate LSP progress message if it's too long
+                --             local lsp_progress = vim.fn["lsp-status"].status()
+                --             return lsp_progress and #lsp_progress > 50 and lsp_progress:sub(1, 47) .. "..."
+                --                 or lsp_progress
+                --         end,
+                --     },
+                -- },
                 lualine_c = {
                     "filename",
-                    require("lsp-status").status(),
-                    {
-                        function()
-                            local noice_status = require("noice").api.statusline.mode.get()
-                            -- Truncate noice status if it's too long
-                            return noice_status and #noice_status > 50 and noice_status:sub(1, 47) .. "..."
-                                or noice_status
-                        end,
-                        cond = require("noice").api.statusline.mode.has,
-                        color = { fg = "#ff9e64" },
-                    },
-                    {
-                        function()
-                            -- Truncate LSP progress message if it's too long
-                            local lsp_progress = vim.fn["lsp-status"].status()
-                            return lsp_progress and #lsp_progress > 50 and lsp_progress:sub(1, 47) .. "..."
-                                or lsp_progress
-                        end,
-                    },
+                    function()
+                        local msg = 'No Active Lsp'
+                        local icon = ' LSP:'
+                        local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+                        local clients = vim.lsp.get_clients()
+                        if next(clients) == nil then
+                            return msg
+                        end
+                        for _, client in ipairs(clients) do
+                            local filetypes = client.config.filetypes
+                            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                                return icon .. client.name
+                            end
+                        end
+                        return msg
+                    end,
+                    -- color = { fg = '#ffffff', gui = 'bold' },
                 },
                 lualine_x = { "encoding", "fileformat", "filetype" },
                 lualine_y = { "progress" },
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,38 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = function()
-		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"elixir",
-				"heex",
-				"javascript",
-				"typescript",
-				"tsx",
-				"html",
-				"python",
-				"php",
-				"java",
-			},
-			auto_install = true,
-			sync_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = true,
-			},
-			indent = {
-				enable = true,
-			},
-		})
-	end,
+        configs.setup({
+            ensure_installed = {
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "elixir",
+                "heex",
+                "javascript",
+                "typescript",
+                "tsx",
+                "html",
+                "python",
+                "php",
+                "java",
+            },
+            auto_install = true,
+            sync_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+            },
+            indent = {
+                enable = true,
+            },
+            autotag = {
+                enable = false,
+            },
+        })
+    end,
 }
