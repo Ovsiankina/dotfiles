ba82aafa72e4714c51a90b00a1136c080a0c1bcc
ovsiankina <ovsiankina@proton.me>
2025-01-27 15:43:27 +0100

    style(lualine): small style improvement + refactor

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,46 +4,18 @@ return {
         opts = {
             options = {
                 theme = "catppuccin",
+                globalstatus = true,
             },
             sections = {
                 lualine_a = { "mode" },
                 lualine_b = { "branch", "diff", "diagnostics" },
-                -- lualine_c = {
-                -- 	"filename",
-                -- 	require("lsp-status").status(),
-                -- 	{
-                -- 		require("noice").api.statusline.mode.get,
-                -- 		cond = require("noice").api.statusline.mode.has,
-                -- 		color = { fg = "#ff9e64" },
-                -- 	},
-                -- 	"lsp_progress", -- Add lsp_progress here
-                -- },
-                -- lualine_c = {
-                --     "filename",
-                --     require("lsp-status").status(),
-                --     {
-                --         function()
-                --             local noice_status = require("noice").api.statusline.mode.get()
-                --             -- Truncate noice status if it's too long
-                --             return noice_status and #noice_status > 50 and noice_status:sub(1, 47) .. "..."
-                --                 or noice_status
-                --         end,
-                --         cond = require("noice").api.statusline.mode.has,
-                --         color = { fg = "#ff9e64" },
-                --     },
-                --     {
-                --         function()
-                --             -- Truncate LSP progress message if it's too long
-                --             local lsp_progress = vim.fn["lsp-status"].status()
-                --             return lsp_progress and #lsp_progress > 50 and lsp_progress:sub(1, 47) .. "..."
-                --                 or lsp_progress
-                --         end,
-                --     },
-                -- },
                 lualine_c = {
-                    "filename",
+                    "filetype",
+                    "filename"
+                },
+                lualine_x = {
                     function()
-                        local msg = 'No Active Lsp'
+                        local msg = ' LSP UNACTIVE'
                         local icon = ' LSP:'
                         local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
                         local clients = vim.lsp.get_clients()
@@ -58,9 +30,9 @@ return {
                         end
                         return msg
                     end,
-                    -- color = { fg = '#ffffff', gui = 'bold' },
+                    -- "encoding",
+                    "fileformat",
                 },
-                lualine_x = { "encoding", "fileformat", "filetype" },
                 lualine_y = { "progress" },
                 lualine_z = { "location" },
             },
