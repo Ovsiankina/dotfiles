a363ec3e3cc59f566afeee6451a4b4508a33e7f2
ovsiankina <ovsiankina@proton.me>
2025-01-28 10:10:44 +0100

    feat(nvim): hardtime: make vim unusable :)

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {}
+    },
+    {
+        'antonk52/bad-practices.nvim',
+    }
+}
