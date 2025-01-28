06190a564443102d91728854223aeb178b0de874
ovsiankina <ovsiankina@proton.me>
2025-01-28 10:37:28 +0100

    feat(nvim): Nvim resize panel plugin

--- .config/nvim/lua/ovsiankina/remap.lua
+++ .config/nvim/lua/ovsiankina/remap.lua
@@ -18,20 +18,15 @@ keymap("n", "<c-j>", "wincmd j<CR>")
 keymap("n", "<c-h>", "wincmd h<CR>")
 keymap("n", "<c-l>", "wincmd l<CR>")
 
---── Resize panels using Ctrl+Shift+<hjkl> ───────────────────────────
-keymap("n", "<C-S-k>", ":resize +2<CR>", { noremap = true, desc = "Increase height" })
-keymap("n", "<C-S-j>", ":resize -2<CR>", { noremap = true, desc = "Decrease height" })
-keymap("n", "<C-S-h>", ":vertical resize -2<CR>", { noremap = true, desc = "Decrease width" })
-keymap("n", "<C-S-l>", ":vertical resize +2<CR>", { noremap = true, desc = "Increase width" })
-
 ---------- Quick save ----------
-keymap("n", "<leader>w", ":w<CR>", {desc = "Quick save"})
+keymap("n", "<leader>w", ":w<CR>", { desc = "Quick save" })
 
 ---------- Remap "+y to <c-v> ----------
-keymap("n","<C-y>", '"+y', {desc = "Remap +y to <c-v>"})
+keymap("n", "<C-y>", '"+y', { desc = "Remap +y to <c-v>" })
 
 -- Compile Arduino sketch with arduino-cli
 keymap("n", "<leader>ac", ":!arduino-cli compile --fqbn arduino:avr:uno %:p:h<CR>", { desc = "Compile Sketch" })
 
 -- Upload Arduino sketch to the board
-keymap("n", "<leader>au", ":!arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno %:p:h<CR>", { desc = "Upload Sketch" })
+keymap("n", "<leader>au", ":!arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno %:p:h<CR>",
+    { desc = "Upload Sketch" })
