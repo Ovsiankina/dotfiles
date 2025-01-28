b8291e2d4fe68f77af79f3806e1ab45dedd02b72
ovsiankina <ovsiankina@proton.me>
2025-01-28 09:03:23 +0100

    feat: markview, hybrid md,latex and yaml previewer

---
+++ .config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,8 @@
+-- For `plugins/markview.lua` users.
+return {
+    "OXY2DEV/markview.nvim",
+
+    -- Do not lazy load this plugin as it is already lazy-loaded.
+    -- Lazy-loading will cause more time for the previews to load when starting Neovim.
+    lazy = false
+};
