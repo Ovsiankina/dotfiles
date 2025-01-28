8fd2a044d0a884d5c3507578169a840552c5a747
ovsiankina <ovsiankina@proton.me>
2025-01-28 09:18:47 +0100

    feat(nvim): zoxide telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,6 +10,9 @@ return {
         dependencies = { "nvim-telescope/telescope-file-browser.nvim" },
     },
     {
+        'jvgrootveld/telescope-zoxide'
+    },
+    {
         "nvim-telescope/telescope-ui-select.nvim",
         keys = {
             { "<leader>f",   group = "Telescope" },
@@ -79,6 +82,7 @@ return {
                 vim.api.nvim_set_hl(0, group, colors)
             end
 
+            local z_utils = require("telescope._extensions.zoxide.utils")
             require("telescope").setup({
                 defaults = {
                     mappings = {
@@ -101,6 +105,24 @@ return {
                             -- even more opts
                         }),
                     },
+                    zoxide = {
+                        prompt_title = "[ Walking on the shoulders of TJ ]",
+                        mappings = {
+                            default = {
+                                after_action = function(selection)
+                                    print("Update to (" .. selection.z_score .. ") " .. selection.path)
+                                end
+                            },
+                            ["<C-s>"] = {
+                                before_action = function(selection) print("before C-s") end,
+                                action = function(selection)
+                                    vim.cmd.edit(selection.path)
+                                end
+                            },
+                            -- Opens the selected entry in a new split
+                            ["<C-q>"] = { action = z_utils.create_basic_command("split") },
+                        },
+                    }
                 },
             })
             require("telescope").load_extension("ui-select")
