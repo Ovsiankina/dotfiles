c892f786309a4f96284ed86a42dfd627a5639c90
ovsiankina <ovsiankina@proton.me>
2025-01-30 13:37:48 +0100

    hotfix(nvim): none-ls error msg fix

--- .config/nvim/lua/plugins/neogen.lua
+++ .config/nvim/lua/plugins/neogen.lua
@@ -1,45 +1,45 @@
 return {
-	"danymat/neogen",
-	opts = {
-		languages = {
-			c = {
-				template = {
-					annotation_convention = "xmldoc", -- for a full list of annotation_conventions, see supported-languages below,
-				},
-			},
-		},
-	},
-	keys = {
-		{
-			"<leader>n",
-			"",
-			mode = "n",
-			desc = "Annotation generation",
-		},
-		{
-			"<leader>nf",
-			":lua require('neogen').generate()<CR>",
-			mode = "n",
-			desc = "Generate annotation for [f]unctions",
-		},
-		{
-			"<leader>nc",
-			":lua require('neogen').generate({ type = 'class' })<CR>",
-			mode = "n",
-			desc = "Generate annotation for [c]lasses",
-		},
-		{
-			"<leader>nt",
-			":lua require('neogen').generate({ type = 'type' })<CR>",
-			mode = "n",
-			desc = "Generate annotation for [t]ypes",
-		},
-		{
-			"<leader>nF",
-			":lua require('neogen').generate({ type = 'file' })<CR>",
-			mode = "n",
-			desc = "Generate annotation for [F]ile",
-		},
-	},
+    "danymat/neogen",
+    opts = {
+        languages = {
+            c = {
+                template = {
+                    annotation_convention = "xmldoc", -- for a full list of annotation_conventions, see supported-languages below,
+                },
+            },
+        },
+    },
+    keys = {
+        {
+            "<leader>Cn",
+            "",
+            mode = "n",
+            desc = "Annotation generation",
+        },
+        {
+            "<leader>nf",
+            ":lua require('neogen').generate()<CR>",
+            mode = "n",
+            desc = "Generate annotation for [f]unctions",
+        },
+        {
+            "<leader>nc",
+            ":lua require('neogen').generate({ type = 'class' })<CR>",
+            mode = "n",
+            desc = "Generate annotation for [c]lasses",
+        },
+        {
+            "<leader>nt",
+            ":lua require('neogen').generate({ type = 'type' })<CR>",
+            mode = "n",
+            desc = "Generate annotation for [t]ypes",
+        },
+        {
+            "<leader>nF",
+            ":lua require('neogen').generate({ type = 'file' })<CR>",
+            mode = "n",
+            desc = "Generate annotation for [F]ile",
+        },
+    },
 }
 -- TODO: add cs xml annotation
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,80 +6,80 @@
 -- }
 
 return {
-    -- null-ls configuration
-    {
-        -- Hook non-LSP sources to LSP client to for exemple expend code actions
-        "nvimtools/none-ls.nvim",
-        dependencies = {
-            "nvimtools/none-ls-extras.nvim",
-        },
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
+	-- null-ls configuration
+	{
+		-- Hook non-LSP sources to LSP client to for exemple expend code actions
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
 
-                    -- Code actions
-                    null_ls.builtins.code_actions.xo,             -- JS (eslint wrapper)
-                    null_ls.builtins.code_actions.gitsigns,       -- git operations
-                    null_ls.builtins.code_actions.refactoringm,   -- go, js, python
-                    null_ls.builtins.code_actions.ts_node_action, -- treesitter
+				sources = {
 
-                    -- Completions
-                    null_ls.builtins.completion.luasnip,
-                    null_ls.builtins.completion.spell,
+					-- Code actions
+					-- null_ls.builtins.code_actions.xo,             -- JS (eslint wrapper)
+					null_ls.builtins.code_actions.gitsigns, -- git operations
+					-- null_ls.builtins.code_actions.refactoringm,   -- go, js, python
+					null_ls.builtins.code_actions.ts_node_action, -- treesitter
 
-                    -- Diagnostics
-                    null_ls.builtins.diagnostics.actionlint,   -- GitHub actions workglow
-                    null_ls.builtins.diagnostics.checkmake,    -- Make file
-                    null_ls.builtins.diagnostics.chktex,       --LaTeX
-                    null_ls.builtins.diagnostics.clang_check,  -- c, cpp
-                    null_ls.builtins.diagnostics.cppcheck,     -- static c/cpp analysis
-                    null_ls.builtins.diagnostics.cpplint,      -- cpp google style
-                    null_ls.builtins.diagnostics.djlint,       -- Django
-                    null_ls.builtins.diagnostics.hadolint,     -- Dockerfile
-                    null_ls.builtins.diagnostics.luacheck,     -- Lua
-                    null_ls.builtins.diagnostics.markdownlint, -- Markdown
+					-- Completions
+					null_ls.builtins.completion.spell,
 
-                    -- Formating
-                    null_ls.builtins.formatting.black,   -- Python
-                    null_ls.builtins.formatting.stylua,  -- Lua
-                    null_ls.builtins.formatting.rustfmt, --rust
-                },
+					-- Diagnostics
+					null_ls.builtins.diagnostics.actionlint, -- GitHub actions workglow
+					null_ls.builtins.diagnostics.checkmake, -- Make file
+					-- null_ls.builtins.diagnostics.chktex,       --LaTeX
+					-- null_ls.builtins.diagnostics.clang_check,  -- c, cpp
+					null_ls.builtins.diagnostics.cppcheck, -- static c/cpp analysis
+					-- null_ls.builtins.diagnostics.cpplint,      -- cpp google style
+					null_ls.builtins.diagnostics.djlint, -- Django
+					null_ls.builtins.diagnostics.hadolint, -- Dockerfile
+					-- null_ls.builtins.diagnostics.luacheck,     -- Lua
+					null_ls.builtins.diagnostics.markdownlint, -- Markdown
 
-                -- Exemple on how to config a source (C_A.gitsigns here)
-                -- Goal: filter out blame actions
-                --
-                -- local gitsigns = null_ls.builtins.code_actions.gitsigns.with({
-                --     config = {
-                --         filter_actions = function(title)
-                --
-                --             return title:lower():match("blame") == nil
-                --         end,
-                --     },
-                -- })
-                debug = true,
-            })
-        end,
+					-- Formating
+					null_ls.builtins.formatting.black, -- Python
+					null_ls.builtins.formatting.stylua, -- Lua
+					-- null_ls.builtins.formatting.rustfmt, --rust
+				},
 
-        --          ╭─────────────────────────────────────────────────────────╮
-        --          │                          Remap                          │
-        --          ╰─────────────────────────────────────────────────────────╯
-        keys = {
-            { "fc", vim.lsp.buf.format, mode = "n", desc = "Format code" },
-        },
-    },
+				-- Exemple on how to config a source (C_A.gitsigns here)
+				-- Goal: filter out blame actions
+				--
+				-- local gitsigns = null_ls.builtins.code_actions.gitsigns.with({
+				--     config = {
+				--         filter_actions = function(title)
+				--
+				--             return title:lower():match("blame") == nil
+				--         end,
+				--     },
+				-- })
+				debug = true,
+			})
+		end,
 
-    -- Mason-null-ls configuration
-    -- {
-    --     "jay-babu/mason-null-ls.nvim",
-    --     event = { "BufReadPre", "BufNewFile" },
-    --     dependencies = {
-    --         "williamboman/mason.nvim",
-    --         "nvimtools/none-ls.nvim",
-    --     },
-    --     opts = {
-    --         -- ensure_installed = null_ls_sources,
-    --         automatic_installation = true,
-    --     },
-    -- },
+		--          ╭─────────────────────────────────────────────────────────╮
+		--          │                          Remap                          │
+		--          ╰─────────────────────────────────────────────────────────╯
+		keys = {
+			{ "fc", vim.lsp.buf.format, mode = "n", desc = "Format code" },
+		},
+	},
+
+	-- Mason-null-ls configuration
+	-- {
+	--     "jay-babu/mason-null-ls.nvim",
+	--     event = { "BufReadPre", "BufNewFile" },
+	--     dependencies = {
+	--         "williamboman/mason.nvim",
+	--         "nvimtools/none-ls.nvim",
+	--     },
+	--     opts = {
+	--         -- ensure_installed = null_ls_sources,
+	--         automatic_installation = true,
+	--     },
+	-- },
 }
