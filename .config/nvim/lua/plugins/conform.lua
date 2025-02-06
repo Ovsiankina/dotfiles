95e5778da100ace53f727ff27ca973626a6b5879
ovsiankina <ovsiankina@proton.me>
2025-02-06 14:47:09 +0100

    feat(nvim): css, ts, jsx, tsx, json and md formatters added

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,12 +3,17 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			-- Conform will run multiple formatters sequentially
 			python = { "isort", "black" },
-			-- You can customize some of the format options for the filetype (:help conform.format)
-			rust = { "rustfmt", lsp_format = "fallback" },
-			-- Conform will run the first available formatter
-			javascript = { "prettierd", "prettier", stop_after_first = true },
+			rust = { "rustfmt" },
+			javascript = { "prettierd", "prettier" },
+			javascriptreact = { "prettierd", "prettier" },
+			typescript = { "prettierd", "prettier" },
+			typescriptreact = { "prettierd", "prettier" },
+			json = { "prettierd", "prettier" },
+			html = { "prettierd", "prettier" },
+			css = { "prettierd", "prettier" },
+			markdown = { "prettierd", "prettier" },
+			yaml = { "prettierd", "prettier" },
 		},
 	},
 }
