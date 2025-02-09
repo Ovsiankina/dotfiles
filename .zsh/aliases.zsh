b3a94f0d3a066d8ba7deef942a8e0eaabf83d1bb
ovsiankina <ovsiankina@proton.me>
2025-02-09 08:51:15 +0100

    feat(zsh): grep aliased to rg

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,7 +2,8 @@
 #          │                         Aliases                          │
 #          ╰──────────────────────────────────────────────────────────╯
 # alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions'
-alias grep='grep --color=auto'
+# alias grep='grep --color=auto'
+alias grep='rg'
 # alias sp='sudo pacman'
 alias cat='bat'
 alias clip='wl-copy'
