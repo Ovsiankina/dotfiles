2083c36f2898d3d9a235fcfc2653b0a1e289de33
ovsiankina <ovsiankina@proton.me>
2025-02-28 10:52:05 +0100

    feat(nvim): better bookmarks

--- .config/nvim/lua/plugins/bookmark.lua
+++ .config/nvim/lua/plugins/bookmark.lua
@@ -1,10 +1,25 @@
 return {
-  "zongben/navimark.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim"
-  },
-  config = function()
-    require("navimark").setup()
-  end,
+	"zongben/navimark.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+		keymap = {
+			base = {
+				mark_toggle = "<leader>mm",
+				mark_add = "<leader>ma",
+				mark_remove = "<leader>mr",
+				goto_next_mark = "]m",
+				goto_prev_mark = "[m",
+				open_mark_picker = "<leader>fm",
+			},
+		},
+		sign = {
+			text = "",
+			color = "#FF0000",
+		},
+		persist = true,
+	},
+	config = function()
+		require("navimark").setup()
+	end,
 }
